4ce8b2f8faabed8a719a6b78f9c86c508a316850
Esmail EL BoB <esmailelbob01124320019@pm.me>
2019-04-23 04:23:13 +0200

    added new things!
    updated the main page of the website

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
 function loginme() {
   window.open("pages/data/index");
 }
+
+function showTheButton() {
+  let login = document.getElementById("login");
+      login.style.display = "block";
+ }
+
