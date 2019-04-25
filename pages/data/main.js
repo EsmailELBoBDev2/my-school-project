88c558c1c72026c4b57a9a4d27ed5d5d1e40d993
Esmail EL BoB <esmailelbob01124320019@pm.me>
2019-04-25 04:43:28 +0200

    updated in timer function again!

--- pages/data/main.js
+++ pages/data/main.js
@@ -11,6 +11,7 @@ firebase.initializeApp(config);
 let dataRef = firebase.database().ref("data");
 
 dataRef.on("child_added", snap => {
+
   let name = snap.child("name").val();
   let medicName = snap.child("medicName").val();
   let medicTime = snap.child("medicTime").val();
@@ -19,21 +20,37 @@ dataRef.on("child_added", snap => {
   $("#dataShow").append("<tr><td>" + "Name: " + name + "</td><td>" + "&nbsp;&nbsp;--&nbsp;&nbsp;" + " Medic Name: " + medicName + "</td><td>" + "&nbsp;&nbsp;--&nbsp;&nbsp;" + " Medic Time: " + medicTime + "</td><td>" + "&nbsp;&nbsp;--&nbsp;&nbsp;" + " Notes: " + notes + "<br>");
 
 
-setInterval(function(){ 
-location.reload();
-}, 300000);
-setTimeout(function timer() {
 let date = new Date();
 let currentHour = date.getHours();
-    if (medicTime == currentHour) {
-      alert("it's " + medicName + " Time!" + "(It's " + medicTime + " Time!)");
-    }
-  }, 1)
+console.log(currentHour);
+
+// This will not work in snippet because sandboxing
+$('#test').click(
+   () => localStorage.setItem('start', new Date().getTime()) // Time in ms from 1 Jan 1970
+  )
+
+checkTime = () => {
+  //if no date set in localstorage...
+  if(!localStorage.getItem('start')) {
+    clearInterval(myInterval);
+  }
+  const currentTime = new Date().getTime();
+  const startTime = localStorage.getItem('start');
+  if(currentTime - startTime > 60000){ // if 60s are passed
+      clearInterval(myInterval); // Unregister interval
+if (medicTime == currentHour) {
+      console.log("it's " + medicName + " Time!" + "(It's " + medicTime + " Time!)");
+  }
+
+  }
+}
+
+// Run the check every seconds
+const myInterval = setInterval(checkTime, 1000);
+
+    
 
 })
 
 
-let date = new Date();
-let currentHour = date.getHours();
-  console.log(currentHour);
 
