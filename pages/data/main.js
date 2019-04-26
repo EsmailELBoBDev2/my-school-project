517962ccd645f975c676c4ab48827817bae79b13
Esmail EL BoB <esmailelbob01124320019@pm.me>
2019-04-26 17:52:26 +0200

    updates!

--- pages/data/main.js
+++ pages/data/main.js
@@ -19,36 +19,49 @@ dataRef.on("child_added", snap => {
 
   $("#dataShow").append("<tr><td>" + "Name: " + name + "</td><td>" + "&nbsp;&nbsp;--&nbsp;&nbsp;" + " Medic Name: " + medicName + "</td><td>" + "&nbsp;&nbsp;--&nbsp;&nbsp;" + " Medic Time: " + medicTime + "</td><td>" + "&nbsp;&nbsp;--&nbsp;&nbsp;" + " Notes: " + notes + "<br>");
 
-
 let date = new Date();
 let currentHour = date.getHours();
-console.log(currentHour);
+console.log("Your current PC time is: " + currentHour);
 
-// This will not work in snippet because sandboxing
-$('#test').click(
-   () => localStorage.setItem('start', new Date().getTime()) // Time in ms from 1 Jan 1970
-  )
+setInterval(function(){ 
+location.reload();
+}, 300000);
 
-checkTime = () => {
-  //if no date set in localstorage...
-  if(!localStorage.getItem('start')) {
-    clearInterval(myInterval);
-  }
-  const currentTime = new Date().getTime();
-  const startTime = localStorage.getItem('start');
-  if(currentTime - startTime > 60000){ // if 60s are passed
-      clearInterval(myInterval); // Unregister interval
+setTimeout(function(){ 
 if (medicTime == currentHour) {
-      console.log("it's " + medicName + " Time!" + "(It's " + medicTime + " Time!)");
-  }
+console.log("it's " + medicName + " Time!");
+    const alarm = new Audio()
+    alarm.src = 'alarm.mp3'
+    alarm.play()
+  $("#testo").append("<tr><td>" + "Name: " + name + "</td><td>" + "&nbsp;&nbsp;--&nbsp;&nbsp;" + " Medic Name: " + medicName + "</td><td>" + "&nbsp;&nbsp;--&nbsp;&nbsp;" + " Medic Time: " + medicTime + "</td><td>" + "&nbsp;&nbsp;--&nbsp;&nbsp;" + " Notes: " + notes + "<br>");
+}
+}, 1 );
 
-  }
+// Get the modal
+var modal = document.getElementById('myModal');
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
 }
 
-// Run the check every seconds
-const myInterval = setInterval(checkTime, 1000);
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
 
-    
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
 
 })
 
