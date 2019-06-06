febe64c8854da5e91fcb057c987b7d75f298070f
Esmail EL BoB <esmailelbob@protonmail.com>
2019-06-06 07:39:24 +0200

    updated

--- main.js
+++ main.js
@@ -1,14 +1,48 @@
-function loginme() {
-  window.open("pages/data/index");
-}
+let name = document.getElementById("name").value;
+let medicName = document.getElementById("medic-name").value;
+let medicTime = document.getElementById("medic-time").value;
+let notes = document.getElementById("notes").value;
+var database;
+// Your web app's Firebase configuration
+ var firebaseConfig = {
+    apiKey: "AIzaSyB444udd_UeONooSUbjOplVqrElpHv4Hgc",
+    authDomain: "my-first-project-9a87e.firebaseapp.com",
+    databaseURL: "https://my-first-project-9a87e.firebaseio.com",
+    projectId: "my-first-project-9a87e",
+    storageBucket: "",
+    messagingSenderId: "15533904472",
+    appId: "1:15533904472:web:b260608b9c15e8f3"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
 
-function showTheButton() {
-  let login = document.getElementById("login");
-      login.style.display = "block";
- }
 
-function hideTheButton() {
-  let login = document.getElementById("login");
-      login.style.display = "none";
-alert("Well, I Love Robots! But Sorry You Are Not Welcome Here")
- }
+  database = firebase.database();
+
+//    document.getElementById("submit").addEventListener("click", function(event){
+//     event.preventDefault()
+//     var data = {
+//         name: name.value,
+//         MedicName: medicName.value,
+//         MedicTime: medicTime.value,
+//         Notes: notes.value
+//       }
+//     var MedicalData = database.ref('Medical Data');
+//     MedicalData.push(data);
+//   });
+
+
+
+
+       document.getElementById("submit").addEventListener("click", function(event){
+    event.preventDefault()
+    var data = {
+        name: name,
+        MedicName: medicName,
+        MedicTime: medicTime,
+        Notes: notes
+      }
+    var MedicalData = database.ref('Medical Data');
+    MedicalData.push(data);
+  });
+
