cb18665a17f243b47c9dbfe7f1c70d60d4b738f2
Esmail EL BoB <esmailelbob@protonmail.com>
2019-06-07 10:37:39 +0200

    Encryption!
    Added Encryption Before Send Data In DB

--- main.js
+++ main.js
@@ -1,28 +1,39 @@
-let name = document.getElementById("name").value;
-let medicName = document.getElementById("medic-name").value;
-let medicTime = document.getElementById("medic-time").value;
-let notes = document.getElementById("notes").value;
+var firebaseConfig = {
+  apiKey: "AIzaSyB444udd_UeONooSUbjOplVqrElpHv4Hgc",
+  authDomain: "my-first-project-9a87e.firebaseapp.com",
+  databaseURL: "https://my-first-project-9a87e.firebaseio.com",
+  projectId: "my-first-project-9a87e",
+  storageBucket: "",
+  messagingSenderId: "15533904472",
+  appId: "1:15533904472:web:b260608b9c15e8f3"
+};
+firebase.initializeApp(firebaseConfig);
+let MedicalData = firebase.database().ref('Medical Data');
 
- var firebaseConfig = {
-    apiKey: "AIzaSyB444udd_UeONooSUbjOplVqrElpHv4Hgc",
-    authDomain: "my-first-project-9a87e.firebaseapp.com",
-    databaseURL: "https://my-first-project-9a87e.firebaseio.com",
-    projectId: "my-first-project-9a87e",
-    storageBucket: "",
-    messagingSenderId: "15533904472",
-    appId: "1:15533904472:web:b260608b9c15e8f3"
-  };
-  firebase.initializeApp(firebaseConfig);
-  let MedicalData = firebase.database().ref('Medical Data');
+var encryptedName = CryptoJS.AES.encrypt(document.getElementById("name").value, 'lol');
+var encryptedName = CryptoJS.AES.encrypt(document.getElementById("medic-name").value, 'lol');
+var encryptedName = CryptoJS.AES.encrypt(document.getElementById("medic-time").value, 'lol');
+var encryptedName = CryptoJS.AES.encrypt(document.getElementById("notes").value, 'lol');
 
-       document.getElementById("submit").addEventListener("click", function(event){
-    event.preventDefault()
-    var data = {
-        name: name,
-        MedicName: medicName,
-        MedicTime: medicTime,
-        Notes: notes
-      }
-    MedicalData.push(data);
-  });
 
+document.getElementById("encrypt").addEventListener("click", function (event) {
+  event.preventDefault()
+  document.getElementById("encryptednameHTML").innerHTML = encryptedName;
+  document.getElementById("encryptedmedicnameHTML").innerHTML = encryptedName;
+  document.getElementById("encryptedmedictimeHTML").innerHTML = encryptedName;
+  document.getElementById("encryptednotesHTML").innerHTML = encryptedName;
+
+
+});
+
+
+document.getElementById("submit").addEventListener("click", function (event) {
+  event.preventDefault()
+  var data = {
+    name: document.getElementById("encryptednameHTML").innerHTML,
+    MedicName: document.getElementById("encryptedmedicnameHTML").innerHTML,
+    MedicTime: document.getElementById("encryptedmedictimeHTML").innerHTML,
+    Notes: document.getElementById("encryptednotesHTML").innerHTML
+  }
+  MedicalData.push(data);
+});
